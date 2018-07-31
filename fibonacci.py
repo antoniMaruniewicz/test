645ab707823160e6202af9d55604decb0d733ff6
Antoni Maruniewicz <antonimaruniewicz@gmail.com>
2018-07-31 11:15:02 +0200

    print function added

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+import sys
+i = 0
+j = 1
+k = 0
+fib = 0
+print("Zmiana2")
+number = int(input("Hown many numbers do you want to display?"))
+print("Fibonnaci sequance:")
+while(i < number):
+    print(fib)
+    fib = j + k
+    j = k
+    k = fib
+    i += 1
+    # to do: format 
